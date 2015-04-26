3700045751456d3f9a980e9893393950af1625e3
Ben Vreeland <vreelb@gmail.com>
2015-04-26 15:05:38 -0400

    Adds NHL abbreviation list.
    I need to look up all 30 "official" three letter abbreviations to compare
    against these.

--- abbrev.php
+++ abbrev.php
@@ -1,7 +1,6 @@
 <?php
 // All CHS abbrev that have CHN counterparts.
 $chs_chn = [
-
 // Atlantic Hockey
 	"afam" => "1",		// Air Force
 	"aicm" => "5",		// American International (AIC)
@@ -14,7 +13,6 @@ $chs_chn = [
 	"ritm" => "49",		// RIT
 	"rmum" => "50",		// Robert Morris
 	"sacm" => "51",		// Sacred Heart
-
 // Big Ten
 	"micm" => "31",		// Michigan
 	"msum" => "32",		// Michigan State
@@ -22,7 +20,6 @@ $chs_chn = [
 	"osum" => "44",		// Ohio State
 	"psum" => "60",		// Penn State
 	"wism" => "58",		// Wisconsin
-
 // ECAC
 	"brnm" => "12",		// Brown
 	"clkm" => "14",		// Clarkson
@@ -36,7 +33,6 @@ $chs_chn = [
 	"stlm" => "53",		// St. Lawrence
 	"unim" => "54",		// Union
 	"yalm" => "59",		// Yale
-
 // Hockey East
 	"bc_m" => "9",		// Boston College (BC)
 	"bu_m" => "10",		// Boston University (BU)
@@ -50,7 +46,6 @@ $chs_chn = [
 	"ndmm" => "43",		// Notre Dame
 	"prvm" => "46",		// Providence
 	"verm" => "55",		// Vermont
-
 // NCHC
 	"cc_m" => "16",		// Colorado College (CC)
 	"denm" => "20",		// Denver
@@ -60,7 +55,6 @@ $chs_chn = [
 	"ndkm" => "40",		// North Dakota
 	"stcm" => "52",		// St. Cloud State
 	"wmum" => "57",		// Western Michigan
-
 // WCHA
 	"alhm" => "2",		// Alabama Huntsville
 	"akam" => "3",		// Alaska Anchorage (UAA)
@@ -72,6 +66,60 @@ $chs_chn = [
 	"mtum" => "33",		// Michigan Tech
 	"mnsm" => "35",		// Minnesota State
 	"nmum" => "42",		// Northern Michigan
+];
 
+$NHL_abbr = [
+// Common Mappings
+	"ANA" => "Anaheim",				// Anaheim		Anaheim Ducks|ANA
+	"ARZ" => "Arizona",				// Arizona		Arizona Coyotes|ARZ
+	"BOS" => "Boston",				// Boston		Boston Bruins|BOS
+	"BUF" => "Buffalo",				// Buffalo		Buffalo Sabres|BUF
+	"CGY" => "Calgary",				// Calgary		Calgary Flames|CGY
+	"CAR" => "Carolina",			// Carolina		Carolina Hurricanes|CAR
+	"CHI" => "Chicago",				// Chicago		Chicago Blackhawks|CHI
+	"COL" => "Colorado",			// Colorado		Colorado Avalanche|COL
+	"CBJ" => "Columbus",			// Columbus		Columbus Blue Jackets|CBJ
+	"DAL" => "Dallas",				// Dallas		Dallas Stars|DAL
+	"DET" => "Detroit",				// Detroit		Detroit Red Wings|DET
+	"EDM" => "Edmonton",			// Edmonton		Edmonton Oilers|EDM
+	"FLA" => "Florida",				// Florida		Florida Panthers|FLA
+	"LAK" => "Los Angeles",			// Los Angeles		Los Angeles Kings|LOS
+	"MIN" => "Minnesota",			// Minnesota		Minnesota Wild|MIN
+	"MON" => "Montreal",			// Montreal		Montreal Canadiens|MON
+	"NSH" => "Nashville",			// Nashville		Nashville Predators|NSH
+	"NJD" => "New Jersey",			// New Jersey		New Jersey Devils|NJD
+	"NYI" => "New York Islanders",	// New York Islanders		New York Islanders|NYI
+	"NYR" => "New York Rangers",	// New York Rangers		New York Rangers|NYR
+	"PHI" => "Philadelphia",		// Philadelphia		Philadelphia Flyers|PHI
+	"PHO" => "Phoenix",				// Phoenix		Phoenix Coyotes|PHO
+	"PIT" => "Pittsburgh",			// Pittsburgh		Pittsburgh Penguins|PIT
+	"OTT" => "Ottawa",				// Ottawa		Ottawa Senators|OTT
+	"STL" => "St. Louis",			// St. Louis		St. Louis Blues|STL
+	"SJS" => "San Jose",			// San Jose		San Jose Sharks|SJS
+	"TAM" => "Tampa Bay",			// Tampa Bay		Tampa Bay Lightning|TAM
+	"TOR" => "Toronto",				// Toronto		Toronto Maple Leafs|TOR
+	"VAN" => "Vancouver",			// Vancouver		Vancouver Canucks|VAN
+	"WSH" => "Washington",			// Washington		Washington Capitals|WAS
+	"WPG" => "Winnipeg",			// Winnipeg		Winnipeg Jets|WPG
+// Inconsistant Mappings
+	"LOS" => "Los Angeles",
+	"MTL" => "Montreal",
+	"PHL" => "Philadelphia",
+	"PHX" => "Phoenix",
+	"TBL" => "Tampa Bay",
+	"WAS" => "Washington",
+	"WIN" => "Winnipeg",
+// Old Mappings
+	// "" => "Atlanta",					// Atlanta Thrashers		Atlanta Thrashers|ATL
+	// "" => "Atlanta Flames",			// Atlanta Flames		Atlanta Flames|ATL
+	// "" => "California",				// California		California Golden Seals|CLF
+	// "" => "Cleveland",				// Cleveland		Cleveland Barons|CLE
+	// "" => "Colorado Rockies",		// Colorado Rockies		Colorado Rockies (NHL)|COR
+	// "" => "Hartford",				// Hartford		Hartford Whalers|HTF
+	// "" => "Kansas City",				// Kansas City		Kansas City Scouts|KCS
+	// "" => "Minnesota North Stars",	// Minnesota North Stars		Minnesota North Stars|MNS
+	// "" => "Oakland",					// Oakland		Oakland Seals|OAK
+	// "" => "Quebec",					// Quebec		Quebec Nordiques|QUE
+	// "" => "Winnipeg Jets",			// Winnipeg		Winnipeg Jets (1972–96)|WIN
 ];
 ?>
