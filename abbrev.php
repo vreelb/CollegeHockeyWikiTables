1e7b8438d221173d0205c92e9104add00350f974
Ben Vreeland <vreelb@gmail.com>
2015-04-26 13:04:27 -0400

    Added all CHS abbrev that have CHN counterparts.

--- abbrev.php
+++ abbrev.php
@@ -1,76 +1,77 @@
 <?php
-$chs_chn = [	// TODO: get all the CHS and CHN abbrs
+// All CHS abbrev that have CHN counterparts.
+$chs_chn = [
 
 // Atlantic Hockey
-// Air-Force/1">Air Force
-// American-International/5">AIC
-// Army/6">Army
-// Bentley/8">Bentley
-// Canisius/13">Canisius
-// Holy-Cross/23">Holy Cross
-// Mercyhurst/28">Mercyhurst
-// Niagara/39">Niagara
-// RIT/49">RIT
-// Robert-Morris/50">Robert Morris
-// Sacred-Heart/51">Sacred Heart
+	"afam" => "1",		// Air Force
+	"aicm" => "5",		// American International (AIC)
+	"armm" => "6",		// Army
+	"benm" => "8",		// Bentley
+	"cnsm" => "13",		// Canisius
+	"hcrm" => "23",		// Holy Cross
+	"mrcm" => "28",		// Mercyhurst
+	"niam" => "39",		// Niagara
+	"ritm" => "49",		// RIT
+	"rmum" => "50",		// Robert Morris
+	"sacm" => "51",		// Sacred Heart
 
 // Big Ten
-// Michigan/31">Michigan
-// Michigan-State/32">Michigan State
-// Minnesota/34">Minnesota
-// Ohio-State/44">Ohio State
-// Penn-State/60">Penn State
-// Wisconsin/58">Wisconsin
+	"micm" => "31",		// Michigan
+	"msum" => "32",		// Michigan State
+	"minm" => "34",		// Minnesota
+	"osum" => "44",		// Ohio State
+	"psum" => "60",		// Penn State
+	"wism" => "58",		// Wisconsin
 
 // ECAC
-// Brown/12">Brown
-// Clarkson/14">Clarkson
-// Colgate/15">Colgate
-// Cornell/18">Cornell
-// Dartmouth/19">Dartmouth
-// Harvard/22">Harvard
-// Princeton/45">Princeton
-// Quinnipiac/47">Quinnipiac
-	"renm" => "48", // Rensselaer
-// St-Lawrence/53">St. Lawrence
-// Union/54">Union
-// Yale/59">Yale
+	"brnm" => "12",		// Brown
+	"clkm" => "14",		// Clarkson
+	"clgm" => "15",		// Colgate
+	"corm" => "18",		// Cornell
+	"darm" => "19",		// Dartmouth
+	"harm" => "22",		// Harvard
+	"prnm" => "45",		// Princeton
+	"quim" => "47",		// Quinnipiac
+	"renm" => "48",		// Rensselaer
+	"stlm" => "53",		// St. Lawrence
+	"unim" => "54",		// Union
+	"yalm" => "59",		// Yale
 
 // Hockey East
-// Boston-College/9">BC
-// Boston-University/10">BU
-// Connecticut/17">Connecticut
-// Maine/25">Maine
-// Massachusetts/27">Massachusetts
-// UMass-Lowell/26">Mass.-Lowell
-// Merrimack/29">Merrimack
-// New-Hampshire/38">UNH
-// Northeastern/41">Northeastern
-// Notre-Dame/43">Notre Dame
-// Providence/46">Providence
-// Vermont/55">Vermont
+	"bc_m" => "9",		// Boston College (BC)
+	"bu_m" => "10",		// Boston University (BU)
+	"conm" => "17",		// Connecticut
+	"mnem" => "25",		// Maine
+	"umam" => "27",		// Massachusetts
+	"umlm" => "26",		// UMass Lowell
+	"merm" => "29",		// Merrimack
+	"unhm" => "38",		// New Hampshire
+	"noem" => "41",		// Northeastern
+	"ndmm" => "43",		// Notre Dame
+	"prvm" => "46",		// Providence
+	"verm" => "55",		// Vermont
 
 // NCHC
-// Colorado-College/16">CC
-// Denver/20">Denver
-// Miami/30">Miami
-// Minnesota-Duluth/36">Minn.-Duluth
-// Nebraska-Omaha/37">Neb.-Omaha
-// North-Dakota/40">North Dakota
-// St-Cloud-State/52">St. Cloud
-// Western-Michigan/57">W. Michigan
+	"cc_m" => "16",		// Colorado College (CC)
+	"denm" => "20",		// Denver
+	"miam" => "30",		// Miami
+	"mndm" => "36",		// Minnesota Duluth
+	"unom" => "37",		// Nebraska Omaha
+	"ndkm" => "40",		// North Dakota
+	"stcm" => "52",		// St. Cloud State
+	"wmum" => "57",		// Western Michigan
 
 // WCHA
-// Alabama-Huntsville/2">Ala.-Huntsville
-// Alaska/4">Alaska
-// Alaska-Anchorage/3">UAA
-// Bemidji-State/7">Bemidji State
-// Bowling-Green/11">Bowling Green
-// Ferris-State/21">Ferris State
-// Lake-Superior/24">Lake Superior
-// Michigan-Tech/33">Michigan Tech
-// Minnesota-State/35">Minnesota State
-// Northern-Michigan/42">N. Michigan
+	"alhm" => "2",		// Alabama Huntsville
+	"akam" => "3",		// Alaska Anchorage (UAA)
+	"akfm" => "4",		// Alaska
+	"bmjm" => "7",		// Bemidji State
+	"bgsm" => "11",		// Bowling Green
+	"fsum" => "21",		// Ferris State
+	"lssm" => "24",		// Lake Superior State
+	"mtum" => "33",		// Michigan Tech
+	"mnsm" => "35",		// Minnesota State
+	"nmum" => "42",		// Northern Michigan
 
 ];
 ?>
